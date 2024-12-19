b394cee7a3bc3b23014231013a1418f31cf69966
alex <>
2024-12-19 13:34:14 +0300

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,34 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginReactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
-export default [
-  eslint.configs.all,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  {
-    "rules": {
-      "no-console": process.env.NODE_ENV === 'DEV' ? 'off': 'error'
+export default tseslint.config({
+  files: ["src/**/*.{ts,tsx}"],
+  languageOptions: {
+    ...eslintPluginReact.configs.flat.all.languageOptions,
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-  {
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.strictTypeChecked,
+    eslintPluginReact.configs.flat.all,
+    eslintPluginReact.configs.flat["jsx-runtime"],
+    eslintPluginPrettier,
+    eslintPluginReactRefresh.configs.vite,
+  ],
+  plugins: {
+    reactHooks: eslintPluginReactHooks.configs.recommended,
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
   },
-]
+});
